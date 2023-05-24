8ce5291d8ed91317452632f338d85a25336b7000
Guilherme Oliveira <guilhermeoc.developer@gmail.com>
2023-05-24 19:08:05 -0300

    ADD: Adicionado pasta

---
+++ DevFreela.API/Models/CreateProjectModel.cs
@@ -0,0 +1,13 @@
+﻿namespace DevFreela.API.Models
+{
+    public class CreateProjectModel
+    {
+        public int id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+
+    }
+}
---
+++ DevFreela.API/Models/UpdateProjectModel.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Net.Http.Headers;
+
+namespace DevFreela.API.Models
+{
+    public class UpdateProjectModel
+    {
+        public string Description { get; set; }
+    }
+}
